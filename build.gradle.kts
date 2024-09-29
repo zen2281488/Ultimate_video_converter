024d0058bb539802438f174748f9b408c00488a6
p.yakovlev <today_na@mail.ru>
2024-09-29 03:03:49 +0300

    New stable version, net.bramp.ffmpeg added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.openjfx.javafxplugin") version "0.0.13"
+    id("edu.sc.seis.launch4j") version "3.0.6"
     application
 }
 
@@ -12,13 +13,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.7.3")
     implementation("org.openjfx:javafx-controls:17")
     implementation("org.openjfx:javafx-fxml:17")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.7.3")
     implementation("net.bramp.ffmpeg:ffmpeg:0.8.0")
-
 }
 
 javafx {
@@ -27,14 +26,27 @@ javafx {
 }
 
 application {
-    // Указываем главный класс приложения
     mainClass.set("com.example.MainApp")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to application.mainClass.get(),
+            "Class-Path" to configurations.runtimeClasspath.get().joinToString(" ") { it.toURI().toString() }
+        )
+    }
+    from(sourceSets.main.get().resources)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
+
+tasks.withType<edu.sc.seis.launch4j.tasks.DefaultLaunch4jTask> {
+    outfile.set("build/launch4j/Converter.exe")
+    mainClassName.set("com.example.MainApp")
+    productName.set("My App")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
 }
